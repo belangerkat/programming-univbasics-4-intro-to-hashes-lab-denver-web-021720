d54854324472162795b142d9b9bca8a0f71d611a
Katie Belanger <belangerkat@gmail.com>
2020-02-07 20:01:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,18 @@
 def new_hash
-  # return an empty hash
+  {}  # return an empty hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  { "Spring" => "is my favorite season." }# return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
+  {name: 'Grace Hopper'}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 def id_generator
+  {id: 7}
   # return a hash with a key :id assigned to positive integer
 end
 
